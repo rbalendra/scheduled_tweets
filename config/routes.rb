a1c2460b9ec86b94fbc23105de79bf943de024d7
Rajeev Balendra <rj.balendra@gmail.com>
2021-04-29 23:11:44 +1000

    Signup form - registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  #GET /About
 
- get "about-us", to:"about#index", as: :about
+ get "about", to:"about#index", as: :about
+
+ get "sign_up", to:"registrations#new"
+ post "sign_up", to:"registrations#create"
+
 
  root to:"main#index"
 
