93ee392d97d92267b00c44961c63e12579842e0c
Rajeev Balendra <rj.balendra@gmail.com>
2021-04-29 22:38:26 +1000

    Validations with regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
+#email:string
+#password_digest:string
+#
+#
+#password:string virtual
+#password_confirmation:string virtual
+
 class User < ApplicationRecord
   has_secure_password # virtual attributes that wont get saved to db. When you submit a password it hashes the password through bcrypt gem inside db.
+
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
 end
