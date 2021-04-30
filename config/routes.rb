cfe13bbda2091fda31dff748d2bcafd90a1355dc
Rajeev Balendra <rj.balendra@gmail.com>
2021-04-30 20:44:22 +1000

    Login Form for user sign up / sign out

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
 
  get "sign_up", to:"registrations#new"
  post "sign_up", to:"registrations#create"
- delete "logout", to: "sessions#destroy"
+ 
+ get "sign_in", to:"sessions#new"
+ post "sign_in", to:"sessions#create"
+
+
+ delete "logout", to:"sessions#destroy"
 
 
  root to:"main#index"
