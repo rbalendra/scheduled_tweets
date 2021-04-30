bf2e64e1e374dfa83e614b14552dfea3ee098898
Rajeev Balendra <rj.balendra@gmail.com>
2021-04-30 19:37:38 +1000

    Loggin out users

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
 
   def index
       if session[:user_id]
-        @user = User.find(session[:user_id])
+        @user = User.find_by(id: session[:user_id])
       end
   end
 
@@ -10,4 +10,4 @@ end
 
 
 # flash[:notice]= "Logged in Successfully" 
-# flash[:alert]= "Invalid email or password"
+# flash[:alert]= "Invalid email or password"``
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+class SessionsController < ApplicationController
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Logged out"
+  end
+
+
+end
